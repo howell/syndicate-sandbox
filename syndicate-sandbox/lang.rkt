2a67b333092c65c2a51ca9a5ff7133ab431a6fef
Sam Caldwell <samcaldwell19@gmail.com>
2025-02-19 15:04:27 -0500

    provide tracing endpoint forms

--- syndicate-sandbox/lang.rkt
+++ syndicate-sandbox/lang.rkt
@@ -1,5 +1,38 @@
 #lang racket/base
 
-(provide (all-from-out syndicate/interactive-lang))
+(provide (all-from-out "tracing-facet-syntax.rkt")
+         (all-from-out syndicate/interactive-lang))
 
-(require (except-in syndicate/interactive-lang #%module-begin))
+(require (only-in "tracing-facet-syntax.rkt"
+                  assert
+                  on-stop
+                  begin/dataflow
+                  stop-when
+                  stop-when-true
+                  during
+
+                  field
+                  #;define-field
+                  define/query-value
+                  define/query-set
+                  define/query-hash
+                  define/query-hash-set
+                  define/query-count))
+
+(require (except-in syndicate/interactive-lang
+                    #%module-begin
+                    assert
+                    on-stop
+                    begin/dataflow
+                    stop-when
+                    stop-when-true
+                    during
+
+                    field
+                    #;define-field
+                    define/query-value
+                    define/query-set
+                    define/query-hash
+                    define/query-hash-set
+                    define/query-count
+                    ))
