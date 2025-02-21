5f3f583f6d70ec656c5d5fe65753a47188f767c7
Sam Caldwell <samcaldwell19@gmail.com>
2025-02-20 20:48:40 -0500

    push combined notifications

--- syndicate-sandbox/server.rkt
+++ syndicate-sandbox/server.rkt
@@ -5,6 +5,7 @@
          phx-port)
 
 (require "session.rkt"
+         "trace-combiner.rkt"
          "dataspace-trace-integrator.rkt"
          racket/date
          net/url
@@ -165,16 +166,11 @@
 (define (push-trace-evt id trace-chan)
   (handle-evt trace-chan
               (lambda (evt)
-                (cond
-                  [(dataspace? evt)
-                   (define seq-no (next-seq-no! id TRACE-TYPE))
-                   (log-sandbox-server-info "~a: Sending session ~a trace step ~a" (timestamp) id seq-no)
-                   (define url (format "/api/sessions/~a/output" id))
-                   (define msg (hash 'type (~a TRACE-TYPE) 'data (dataspace->json evt) 'seq_no seq-no))
-                   (post-http! url msg)]
-                  [else
-                   (log-sandbox-server-info "Non-dataspace trace event")
-                   #t]))))
+                (define seq-no (next-seq-no! id TRACE-TYPE))
+                (log-sandbox-server-info "~a: Sending session ~a trace step ~a" (timestamp) id seq-no)
+                (define url (format "/api/sessions/~a/output" id))
+                (define msg (hash 'type (~a TRACE-TYPE) 'data (notification->json evt) 'seq_no seq-no))
+                (post-http! url msg))))
 
 (define (next-seq-no! id type)
   (define the-session (hash-ref session-envs id))
