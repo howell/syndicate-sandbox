d008d8b7db8acf03b79df01d4bcd2af41729eae1
Sam Caldwell <samcaldwell19@gmail.com>
2025-02-21 07:40:48 -0500

    no longer needed json stuff

--- syndicate-sandbox/tracing-facet-syntax.rkt
+++ syndicate-sandbox/tracing-facet-syntax.rkt
@@ -17,8 +17,7 @@
 
          current-endpoint-notification-handler
 
-         (struct-out endpoint-notification)
-         endpoint-notification->json)
+         (struct-out endpoint-notification))
 
 (require (prefix-in synd: syndicate/actor-lang)
          (prefix-in repl: syndicate/interactive-lang)
@@ -146,98 +145,3 @@
                      (list (endpoint-notification '() 'endpoint (== '(assert 'momma)) (? srcloc?))))))))
 
 
-;; EndpointNotification -> JSExpr
-(define (endpoint-notification->json n)
-  (hash 'facet_id (~a (endpoint-notification-fid n))
-        'type (~a (endpoint-notification-desc n))
-        'detail (endpoint-detail->json (endpoint-notification-detail n))
-        'location (srcloc->json (endpoint-notification-srcloc n))))
-
-;; Any -> JSExpr
-;; Convert endpoint detail to JSON representation
-(define (endpoint-detail->json detail)
-  (cond
-    [(syntax? detail)
-     (~a (syntax->datum detail))]
-    [(field-handle? detail)
-     (field-handle->json detail)]
-    [else (~v detail)]))
-
-;; FieldHandle -> JSExpr
-(define (field-handle->json fh)
-  (match-define (field-handle (field-descriptor name _)) fh)
-  (hash 'field_name (~a name)))
-
-;; Srcloc -> JSExpr
-(define (srcloc->json loc)
-  (match-define (srcloc source line col pos span) loc)
-  (hash 'source (~a source)
-        'line line
-        'column col
-        'position pos
-        'span span))
-
-(module+ test
-  (test-case "endpoint-notification->json"
-    (define sample-srcloc (srcloc "test.rkt" 1 2 3 10))
-    (define sample-notification
-      (endpoint-notification '(facet1) 'endpoint '(assert 'hello) sample-srcloc))
-
-    (check-equal?
-     (endpoint-notification->json sample-notification)
-     (hash 'facet_id "(facet1)"
-           'type "endpoint"
-           'detail "'(assert 'hello)"
-           'location (hash 'source "test.rkt"
-                          'line 1
-                          'column 2
-                          'position 3
-                          'span 10)))
-
-    (check-equal?
-     (endpoint-notification->json
-      (endpoint-notification '(facet2) 'field
-                            (field-handle (field-descriptor 'breakfast #f))
-                            sample-srcloc))
-     (hash 'facet_id "(facet2)"
-           'type "field"
-           'detail (hash 'field_name "breakfast")
-           'location (hash 'source "test.rkt"
-                          'line 1
-                          'column 2
-                          'position 3
-                          'span 10)))
-
-    (check-equal?
-     (endpoint-notification->json
-      (endpoint-notification '(facet3) 'endpoint #'(+ 1 2) sample-srcloc))
-     (hash 'facet_id "(facet3)"
-           'type "endpoint"
-           'detail "(+ 1 2)"
-           'location (hash 'source "test.rkt"
-                          'line 1
-                          'column 2
-                          'position 3
-                          'span 10))))
-
-  (test-case "endpoint-detail->json"
-    (check-equal? (endpoint-detail->json '(hello world))
-                  "'(hello world)")
-    (check-equal? (endpoint-detail->json #'(+ 1 2))
-                  "(+ 1 2)")
-    (check-equal? (endpoint-detail->json
-                   (field-handle (field-descriptor 'test #f)))
-                  (hash 'field_name "test")))
-
-  (test-case "field-handle->json"
-    (check-equal? (field-handle->json
-                   (field-handle (field-descriptor 'breakfast #f)))
-                  (hash 'field_name "breakfast")))
-
-  (test-case "srcloc->json"
-    (check-equal? (srcloc->json (srcloc "test.rkt" 1 2 3 10))
-                  (hash 'source "test.rkt"
-                        'line 1
-                        'column 2
-                        'position 3
-                        'span 10))))
